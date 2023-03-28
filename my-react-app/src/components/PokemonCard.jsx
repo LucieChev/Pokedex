8562c1e6b3fc93dda6fc407f78fcc51982e7c71e
Lucie Chevanche <lchevanche@hotmail.fr>
2023-03-28 11:44:20 +0200

    composant dynamique

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,11 +1,26 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
         <div>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-                <figcaption>bulbasaur</figcaption>
+                {pokemon === pokemonList[0] ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+
+                <figcaption>{pokemon.name}</figcaption>
             </figure>
         </div>
     )
 }
+
 export default PokemonCard;
