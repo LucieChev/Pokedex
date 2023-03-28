3a8f5e841ee7e18e1a82fba2b5bf9fc0a10743dd
Lucie Chevanche <lchevanche@hotmail.fr>
2023-03-28 17:23:27 +0200

    composant with map

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,16 +1,11 @@
-const NavBar = ({ pokemonIndex, setPokemonIndex, pokemonList }) => {
-    const handleClickNext = () => {
-        setPokemonIndex(pokemonIndex + 1)
+const NavBar = ({ setPokemonIndex, pokemonIndex, pokemonList }) => {
 
-    }
-    const handleClickBack = () => {
-        setPokemonIndex(pokemonIndex - 1)
-
-    }
     return (
         <div>
-            {pokemonIndex > 0 ? <button onClick={handleClickBack}>Precedent</button> : <p></p>}
-            {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickNext}>Suivant</button> : <p></p>}
+            {pokemonList.map((pokemon, index) => (
+
+                <button key={index} onClick={() => setPokemonIndex(index)}>{pokemon.name}</button>
+            ))}
         </div>)
 }
 
